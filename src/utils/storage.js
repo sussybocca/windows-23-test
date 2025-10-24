219c1ec71cedc30f6a7b1139c734bb2c0e5b88af
sussybocca <babyyodacutefry@gmail.com>
2025-10-24 08:29:20 -0500

    Update storage.js

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,1 +1,13 @@
+import localforage from "localforage";
 
+export const saveItem = async (key, value) => {
+  await localforage.setItem(key, value);
+};
+
+export const getItem = async (key) => {
+  return await localforage.getItem(key);
+};
+
+export const removeItem = async (key) => {
+  await localforage.removeItem(key);
+};
