734be3b204c792623cf2a65ea61dbfbc55740238
sussybocca <babyyodacutefry@gmail.com>
2025-10-24 08:21:39 -0500

    Update CustomAppPreview.jsx

--- src/components/CustomAppPreview.jsx
+++ src/components/CustomAppPreview.jsx
@@ -1,1 +1,11 @@
+import React from "react";
 
+export default function CustomAppPreview({ code }) {
+  return (
+    <iframe
+      style={{ width: "100%", height: "100%", border: "none" }}
+      sandbox="allow-scripts"
+      srcDoc={`<html><body><script>${code}<\/script></body></html>`}
+    />
+  );
+}
