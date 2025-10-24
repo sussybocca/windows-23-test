31f752f0fa4b8549800f1f13e0b9988a515214e5
sussybocca <babyyodacutefry@gmail.com>
2025-10-23 19:45:42 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Desktop from "./components/Desktop";
 import Cursor from "./components/Cursor";
+import BootScreen from "./components/BootScreen";
 import "./index.css";
 
 export default function App() {
+  const [bootFinished, setBootFinished] = useState(false);
+
   return (
     <>
-      <Desktop />
-      <Cursor />
+      {!bootFinished && <BootScreen onFinish={() => setBootFinished(true)} />}
+      {bootFinished && (
+        <>
+          <Desktop />
+          <Cursor />
+        </>
+      )}
     </>
   );
 }
