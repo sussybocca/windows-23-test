ff24ebfa133f1812259f7266d58dda902a968b59
sussybocca <babyyodacutefry@gmail.com>
2025-10-24 20:48:01 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,16 @@ import Search from "./components/Search";
 import "./index.css";
 
 export default function App() {
-  const [launchFinished, setLaunchFinished] = useState(false);
   const [bootFinished, setBootFinished] = useState(false);
   const [searchOpen, setSearchOpen] = useState(false);
 
   return (
     <>
-      {/* Launch Screen */}
-      {!launchFinished && (
-        <LaunchScreen onFinish={() => setLaunchFinished(true)} />
-      )}
-
       {/* Boot screen */}
-      {launchFinished && !bootFinished && (
-        <BootScreen onFinish={() => setBootFinished(true)} />
-      )}
+      {!bootFinished && <BootScreen onFinish={() => setBootFinished(true)} />}
 
       {/* Main desktop */}
-      {launchFinished && bootFinished && (
+      {bootFinished && (
         <>
           <Desktop />
           <Cursor />
